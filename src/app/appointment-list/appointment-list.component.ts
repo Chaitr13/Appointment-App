8a9053185d0001ea00306f27fd6a86d83f183abc
Chaitra K <chaitrakattu@gmail.com>
2026-04-23 17:52:38 +0530

    Add properties for 2 way binding and some plain html.

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -8,9 +8,7 @@ import { Appointment } from '../models/appointment';
 })
 export class AppointmentListComponent {
   //property used for one way data binding
-  appointment: Appointment = {
-    id: 1,
-    title: 'Take dog for a walk',
-    date: new Date('2026-07-30'),
-  };
+  appointments: Appointment[] = [];
+  newAppointmentTitle: string = '';
+  newAppointmentDate: Date = new Date();
 }
